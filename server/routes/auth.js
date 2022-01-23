11bb27a9abb8f16c5ea9e984db98ed3d84617a62
Oscar Olsson <oscar.olsson94@hotmail.com>
2022-01-23 10:24:16 +0100

    setting up password encryption

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,15 +1,17 @@
 import express from "express";
 import User from "../models/User.js";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
     try {
+        const newPassword = await bcrypt.hash(req.body.password, 10);
         const user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
+            password: newPassword,
         });
         res.json({ status: "ok" });
     } catch (error) {
@@ -23,10 +25,21 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
     const user = await User.findOne({
         email: req.body.email,
-        password: req.body.password,
     });
 
-    if (user) {
+    if (!user) {
+        return res.json({
+            status: "error",
+            error: "No user found with the submitted email",
+        });
+    }
+
+    const isPaswordValid = await bcrypt.compare(
+        req.body.password,
+        user.password
+    );
+
+    if (isPaswordValid) {
         const token = jwt.sign(
             {
                 name: user.name,
@@ -34,10 +47,12 @@ router.post("/login", async (req, res) => {
             },
             process.env.TOKEN_SECRET
         );
-
         return res.json({ status: "ok", token: token });
     } else {
-        return res.json({ status: "error", user: false });
+        return res.json({
+            status: "error",
+            error: "Invalid username and password combination",
+        });
     }
 });
 
